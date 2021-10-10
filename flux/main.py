f7a7fafb4885d9ca4c8b8070aaa4b5b8079a9666
joebockhorst <joe.bockhorst@gmail.com>
2021-10-10 10:07:16 -0500

    new data

--- flux/main.py
+++ flux/main.py
@@ -2,6 +2,9 @@ from flux.core import intragroup_vs_control_table, intergroup_vs_control_table
 
 if __name__ == "__main__":
     filename = "data/092121_HRPFlux_BART_read at 20m.csv"
+    filename = "data/100521_HRPFlux_TVCHPV_5m.csv"
+    filename = "data/100621_HRPFlux_TVCHPV_10m.csv"
+    filename = "data/100621_HRPFlux_TVCHPV_5m.csv"
     intragroup_vs_control_table(filename)
 
     intergroup_vs_control_table(filename, "No Pretreatment")
